43cf7b3f28bd08fbf48c20c774c7e8f4f5e0baca
Violet Eldridge <github@violet-mica.com>
2024-03-14 17:47:03 -0700

    updates

--- wurm_logs/mainA.py
+++ wurm_logs/mainA.py
@@ -19,7 +19,8 @@ class Date(Base):
     year = Column(Integer)
     month = Column(Integer)
     day = Column(Integer)
-
+    def __str__(self):
+        return f"{self.year}-{self.month:02d}-{self.day:02d}"
 
 class LogType(Base):
     __tablename__ = 'log_type'
@@ -36,9 +37,17 @@ class LogMessage(Base):
     hour = Column(Integer)
     minute = Column(Integer)
     second = Column(Integer)
+    log_type_name = Column(String)  # Polymorphic discriminator
+
     user = relationship("User")
     date = relationship("Date")
     log_type = relationship("LogType")
+
+    __mapper_args__ = {
+        'polymorphic_on': log_type_name,
+        'polymorphic_identity': 'log_message',
+    }
+
 
 
 class LogActions(LogMessage):
@@ -46,15 +55,19 @@ class LogActions(LogMessage):
     log_id = Column(Integer, ForeignKey('log_message.log_id'), primary_key=True)  # Inherit log_id
     action = Column(String)
 
+    __mapper_args__ = {
+        'polymorphic_identity': 'action', # chose action instead of log_actions
+    }
+
 
 engine = create_engine('sqlite:///db.sqlite3', echo=False)  # Change database name if needed
 
 def create_tables():
     Base.metadata.create_all(engine)
 
+Session = sessionmaker(bind=engine)
 
 def create_example_data():
-    Session = sessionmaker(bind=engine)
     session = Session()
 
     usernames = [
@@ -68,20 +81,22 @@ def create_example_data():
             session.add(user)
     user1 = session.query(User).where(User.username==usernames[0]).one()
 
-    today = datetime.date.today()
-    date1 = Date(year=today.year, month=today.month, day=today.day)
+    now = datetime.datetime.now()
+    date1 = Date(year=now.year, month=now.month, day=now.day)
     session.add(date1)
 
-    log_type1 = LogType(message='You start {action}.')
-    session.add(log_type1)
+    log_type1 = session.query(LogType).filter_by(message='You start {action}.').first()
+    if not log_type1:
+        log_type1 = LogType(message='You start {action}.')
+        session.add(log_type1)
 
     log_message1 = LogActions(
         user=user1,
         date=date1,
         log_type=log_type1,
-        hour=10,
-        minute=35,
-        second=20,
+        hour=now.hour,
+        minute=now.minute,
+        second=now.second,
         action='to dig'
     )
     session.add(log_message1)
@@ -90,12 +105,17 @@ def create_example_data():
 
 
 def reconstruct_messages():
-    Session = sessionmaker(bind=engine)
     session = Session()
-
-    for action_log in session.query(LogActions):
-        full_message = action_log.log_type.message.format(action=action_log.action)
-        print(f"{action_log.user.username} - {action_log.date} - {full_message}")
+    from sqlalchemy.orm import with_polymorphic
+    # polymorphic_entities = 
+    asdf = with_polymorphic(LogMessage, [LogActions])
+    messages = session.query(asdf)
+    
+    for log_message in messages:    # for log_message in log_messages:
+        if isinstance(log_message, LogActions):  # Checking the type of polymorphic instance
+            action = log_message.action
+            full_message = log_message.log_type.message.format(action=action)
+            print(f"{log_message.user.username} - {log_message.date} {log_message.hour:02d}:{log_message.minute:02d}:{log_message.second:02d} - {full_message}")
 
 
 def main():
